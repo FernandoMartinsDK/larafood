8adff24e920a873490be970c710830efe871e989
unknown <fernandomartinsdk@gmail.com>
2020-05-01 18:32:11 -0300

    aula-12

--- resources/views/admin/pages/plans/_partials/form.blade.php
+++ resources/views/admin/pages/plans/_partials/form.blade.php
@@ -1,14 +1,16 @@
+@include('admin.includes.alerts')
+
 <div class="form-group">
     <label>Nome:</label>
-    <input type="text" name="name" class="form-control" placeholder="Nome do Plano" value="{{ $plan->name ?? '' }}">
+    <input type="text" name="name" class="form-control" placeholder="Nome do Plano" value="{{ $plan->name ?? old('name') }}">
 </div>
 <div class="form-group">
     <label>Preço:</label>
-    <input type="text" name="price" class="form-control" placeholder="Preço do Plano" value="{{ $plan->price ?? '' }}">
+    <input type="text" name="price" class="form-control" placeholder="Preço do Plano" value="{{ $plan->price ?? old('price') }}">
 </div>
 <div class="form-group">
     <label>Descrição:</label>
-    <input type="text" name="description" class="form-control" placeholder="Descrição do Plano" value="{{ $plan->description ?? '' }}">
+    <input type="text" name="description" class="form-control" placeholder="Descrição do Plano" value="{{ $plan->description ?? old('description') }}">
 </div>
 <div class="form-group">
     <button type="submit" class="btn btn-dark">Enviar</button>
